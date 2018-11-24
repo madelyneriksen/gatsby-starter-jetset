4f3c69741607528620e6cd2206ad68fab1aff5c8
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-11-24 15:16:45 -0800

    Added base body classes.

--- src/common/layout.js
+++ src/common/layout.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import Helmet from 'react-helmet';
+import 'tachyons';
 
 
 export default props => (
   <React.Fragment>
+    <Helmet>
+      <body className="mid-gray bg-near-white" />
+    </Helmet>
     {props.children}
   </React.Fragment>
 )
