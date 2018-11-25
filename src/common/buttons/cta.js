3288657890d5383e789e9ff14a86ee7cbbeed3b1
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-11-25 06:08:23 -0800

    Moved buttons to new colors.

--- src/common/buttons/cta.js
+++ src/common/buttons/cta.js
@@ -1,5 +1,5 @@
-const MainCta = "bg-blue near-white ttu b shadow-5 br-pill pv3 bn ph4 mv2 grow";
-const LightCta = "bg-near-white blue ttu b shadow-5 br-pill pv3 bn ph4 mv2 grow";
+const MainCta = "bg-gradient-blue near-white ttu b shadow-5 br-pill pv3 bn ph4 mv2 grow";
+const LightCta = "bg-gradient-white blue ttu b shadow-5 br-pill pv3 bn ph4 mv2 grow";
 
 
 export { MainCta, LightCta };
