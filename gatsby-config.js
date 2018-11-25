4f82619b87e977d08e496257dad966b7f6955576
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-11-24 19:03:22 -0800

    Added content sourcing plugins.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,14 @@
 module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
+    'gatsby-transformer-json',
+    'gatsby-transformer-remark',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: './content/',
+      },
+    },
     {
       resolve: 'gatsby-plugin-web-font-loader',
       options: {
