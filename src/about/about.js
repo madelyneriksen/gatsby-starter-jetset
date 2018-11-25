5c2750d033abec7d2a44302fce5b2e875e2a0818
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-11-24 16:38:22 -0800

    Added about section.

---
+++ src/about/about.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+export default () => (
+  <section
+    id="#about"
+    className="min-vh-100 flex flex-wrap items-center justify-center pa2">
+    <div className="w-100 mw6">
+      <h1 className="f2 navy">JetSet Helps You Launch.</h1>
+      <p className="lh-copy f4">Iusto vitae consequatur rerum. Sed est fugiat sed id saepe consequatur ipsa. Cupiditate explicabo aspernatur rerum est soluta. Natus blanditiis molestiae ipsa occaecati praesentium eum aut nulla.</p>
+      <p className="lh-copy f4">Similique debitis temporibus pariatur illo aut cum inventore. Et quasi tenetur vero consequatur consequatur ex asperiores.</p>
+    </div>
+    <div className="w-100 mw6">
+      {/* TODO: Add Illustration Here. */}
+    </div>
+  </section>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import React from "react"
 import Layout from "../common/layout.js";
 import Hero from "../hero/hero.js";
 import Nav from "../nav/nav.js";
+import About from "../about/about.js";
 
 export default () => (
   <Layout>
     <Hero />
     <Nav />
+    <About />
   </Layout>
 )
