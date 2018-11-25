36d7f7120fff576e335f2ff0831dfd0c29713616
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-11-24 16:39:31 -0800

    Swapped semantic tags to be a bit more clean.

--- src/about/about.js
+++ src/about/about.js
@@ -2,16 +2,16 @@ import React from 'react';
 
 
 export default () => (
-  <section
+  <div
     id="#about"
     className="min-vh-100 flex flex-wrap items-center justify-center pa2">
-    <div className="w-100 mw6">
+    <section className="w-100 mw6">
       <h1 className="f2 navy">JetSet Helps You Launch.</h1>
       <p className="lh-copy f4">Iusto vitae consequatur rerum. Sed est fugiat sed id saepe consequatur ipsa. Cupiditate explicabo aspernatur rerum est soluta. Natus blanditiis molestiae ipsa occaecati praesentium eum aut nulla.</p>
       <p className="lh-copy f4">Similique debitis temporibus pariatur illo aut cum inventore. Et quasi tenetur vero consequatur consequatur ex asperiores.</p>
-    </div>
+    </section>
     <div className="w-100 mw6">
       {/* TODO: Add Illustration Here. */}
     </div>
-  </section>
+  </div>
 )
