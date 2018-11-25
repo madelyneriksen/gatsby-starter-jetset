df903ff2b691ee5f15231b7721b2b1736a3d9c9a
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-11-25 06:49:41 -0800

    Changed spacing on about section.

--- src/about/about.js
+++ src/about/about.js
@@ -18,7 +18,7 @@ export default () => (
     render={data => (
       <div
         id="about"
-        className="min-vh-100 flex flex-wrap items-center justify-around pa2 bg-light-gray">
+        className="min-vh-100 flex flex-wrap items-center justify-around ph2 pv4 bg-light-gray">
         <section className="w-100 mw6">
           <h1 className="f2 navy">{data.markdownRemark.frontmatter.title}</h1>
           <div className="lh-copy f4" dangerouslySetInnerHTML={{__html: data.markdownRemark.html}} />
