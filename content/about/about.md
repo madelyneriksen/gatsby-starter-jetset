9a6cab2dbdf40176a74274a7335aaee0e5c6b204
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-11-24 19:05:15 -0800

    Hooked up about to markdown file.

---
+++ content/about/about.md
@@ -0,0 +1,11 @@
+---
+
+type: "copy"
+name: "about"
+title: "JetSet Helps You Launch."
+
+---
+
+Iusto vitae consequatur rerum. Sed est fugiat sed id saepe consequatur ipsa. Cupiditate explicabo aspernatur rerum est soluta. Natus blanditiis molestiae ipsa occaecati praesentium eum aut nulla.
+
+Similique debitis temporibus pariatur illo aut cum inventore. Et quasi tenetur vero consequatur consequatur ex asperiores.
--- src/about/about.js
+++ src/about/about.js
@@ -1,17 +1,31 @@
 import React from 'react';
+import { StaticQuery, graphql } from 'gatsby';
 
 
 export default () => (
-  <div
-    id="about"
-    className="min-vh-100 flex flex-wrap items-center justify-center pa2">
-    <section className="w-100 mw6">
-      <h1 className="f2 navy">JetSet Helps You Launch.</h1>
-      <p className="lh-copy f4">Iusto vitae consequatur rerum. Sed est fugiat sed id saepe consequatur ipsa. Cupiditate explicabo aspernatur rerum est soluta. Natus blanditiis molestiae ipsa occaecati praesentium eum aut nulla.</p>
-      <p className="lh-copy f4">Similique debitis temporibus pariatur illo aut cum inventore. Et quasi tenetur vero consequatur consequatur ex asperiores.</p>
-    </section>
-    <div className="w-100 mw6">
-      {/* TODO: Add Illustration Here. */}
-    </div>
-  </div>
+  <StaticQuery
+    query={graphql`
+      query {
+        markdownRemark(frontmatter: {type: {eq: "copy"}, name: {eq: "about"}}) {
+          html
+          frontmatter {
+            title
+          }
+        }
+      } 
+    `}
+    render={data => (
+      <div
+        id="about"
+        className="min-vh-100 flex flex-wrap items-center justify-center pa2">
+        <section className="w-100 mw6">
+          <h1 className="f2 navy">{data.markdownRemark.frontmatter.title}</h1>
+          <div className="lh-copy f4" dangerouslySetInnerHTML={{__html: data.markdownRemark.html}} />
+        </section>
+        <div className="w-100 mw6">
+          {/* TODO: Add Illustration Here. */}
+        </div>
+      </div>
+    )}
+  />
 )
