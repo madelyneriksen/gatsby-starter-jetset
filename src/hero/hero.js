e82041709c6995a157570f9d5dd0f164cab5445f
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-11-24 19:15:24 -0800

    Hooked up hero to json file.

--- src/hero/hero.js
+++ src/hero/hero.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import { StaticQuery, graphql } from 'gatsby';
 import Form from '../common/forms/signup.js';
 
 const gradient = "linear-gradient(to top right, indigo, tomato)";
 
 export default () => (
-  <div
-    className="min-vh-100 ph1 pv4 flex flex-column items-center justify-center"
-    id="hero"
-    style={{backgroundImage: gradient}}>
-    <h1 className="near-white f1">Pre-launch Your Project</h1>
-    <Form />
-  </div>
+  <StaticQuery
+    query={graphql`
+      query {
+        heroJson {
+          title
+          body
+        }
+      }
+    `}
+    render={data => (
+      <div
+        className="min-vh-100 ph1 pv4 flex flex-column items-center justify-center"
+        id="hero"
+        style={{backgroundImage: gradient}}>
+        <h1 className="near-white f1 mb2">{data.heroJson.title}</h1>
+        <span className="near-white f4 mw7 tc mb4">{data.heroJson.body}</span>
+        <Form />
+      </div>
+    )}
+  />
 )
