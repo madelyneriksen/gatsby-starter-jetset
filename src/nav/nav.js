1362561e0a884d335c9410daa34da15b2934cd64
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-11-24 16:26:21 -0800

    Added navbar.

---
+++ src/nav/nav.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import { MainCta } from '../common/buttons/cta.js';
+
+
+const navLinkClasses = "no-underline f4 mid-gray dn db-ns";
+
+
+class Nav extends React.Component {
+  render() {
+    return (
+      <nav
+        className="bg-near-white flex items-center justify-around w-100 pv2 top-0"
+        style={{position: "sticky"}}>
+        <h2 className="db mid-gray">
+          <Link to="/" className="no-underline navy">
+            JetSet
+          </Link>
+        </h2>
+        <a href="#about" className={navLinkClasses}>About</a>
+        <a href="#features" className={navLinkClasses}>Features</a>
+        <a href="#contact" className={navLinkClasses}>Contact</a>
+        <a href="#signup" className={MainCta + " no-underline"}>Get Notified</a>
+      </nav>
+      )
+    }
+}
+
+export default Nav;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from "react"
 import Layout from "../common/layout.js";
 import Hero from "../hero/hero.js";
+import Nav from "../nav/nav.js";
 
 export default () => (
   <Layout>
     <Hero />
+    <Nav />
   </Layout>
 )
